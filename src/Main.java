bc5d896f4eef40c52fe7e7cc2ab7105ff8c9967c
demoB33 <vasilenko-s@list.ru>
2022-11-16 17:37:55 +0700

    Условный оператор

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("Можно водить");}
+        if (age < 18) {
+            System.out.println("Нельзя водить");
+        }
+
+
+    }
+}
